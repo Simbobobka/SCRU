b76167038a2eb0b71b366b66bba1eb571c35ae05
Oleh <tomashuk.oleg2005@gmail.com>
2024-09-03 20:32:02 +0300

    Redesign forms

--- parsing/models.py
+++ parsing/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class SavedGoods(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, db_index=True)
-    price = models.CharField(max_length=255, db_index=True)
+    price = models.CharField(max_length=255, db_index=True) 
     url = models.CharField(max_length=255, db_index=True)
    
